0a14b9dd061931dceaf4b013a6eff820699bb5f7
Arjun Timsina <Arjunraaz0@gmail.com>
2024-05-03 20:28:49 +0545

    Problem solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 //import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
 import styles from "./App.module.css";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
@@ -12,17 +10,13 @@ import Contact from './components/Contact/Contact';
 function App() {
   return (
     <div className={styles.App}>
-    <Router basename="/PortFolio">
       <Navbar />
-      <Switch>
-        <Route exact path="/" component={Hero} />
-        <Route path="/about" component={About} />
-        <Route path="/experience" component={Experience} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/contact" component={Contact} />
-      </Switch>
-    </Router>
-  </div>
+      <Hero />
+      <About />
+      <Experience />
+      <Projects />
+      <Contact />
+    </div>
   );
 }
 
