d74ada8525c4cfdba441691d3d64e53ba30d16fb
Arjun Timsina <Arjunraaz0@gmail.com>
2024-05-03 19:10:33 +0545

    problem fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 //import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import styles from "./App.module.css";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
@@ -6,19 +7,24 @@ import About from "./components/About/About";
 import Experience from "./components/Experience/Experience";
 import Projects from './components/Projects/Projects';
 import Contact from './components/Contact/Contact';
+import NotFound from './components/NotFound/NotFound'; // You need to create NotFound component
 
 function App() {
   return (
     <div className={styles.App}>
-      <Navbar />
-      <Hero />
-      <About />
-      <Experience />
-      <Projects />
-      <Contact />
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Hero} />
+          <Route path="/about" component={About} />
+          <Route path="/experience" component={Experience} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/contact" component={Contact} />
+          <Route component={NotFound} /> {/* Handle 404 */}
+        </Switch>
+      </Router>
     </div>
   );
 }
 
 export default App;
-
