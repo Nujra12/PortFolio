ab1a5b4a4f0e12bd8dd8757dc038f69547045c6b
Arjun Timsina <Arjunraaz0@gmail.com>
2024-05-03 20:24:25 +0545

    Problem solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 //import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Switch from "react-router-dom/esm/react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import styles from "./App.module.css";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
